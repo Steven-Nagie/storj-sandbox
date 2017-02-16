acdd2392f10598bf3fa7e13cd6c58cd0a861ecd8
Steven Nagie <sanagie@yahoo.com>
2017-02-16 15:49:59 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict'
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      morgan = require('morgan'),
+      // storj = require('storj.js'),
+      storj = require('storj-lib'),
+      api = 'https://api.storj.io',
+      client = storj.BridgeClient(api),
+      config = require('./config.json'),
+      port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('./public'));
+app.use(morgan('dev'))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+client.createUser({
+  email: config.storjEmail,
+  
+})
+
+// var storjOptions = {
+//   bridge: `http://localhost:${port}`,
+//   basicAuth: {
+//     email: config.storjEmail,
+//     password: config.storjPassword
+//   }
+// }
+
+app.listen(port, function() {
+  console.log(`listening on port ${this.address().port}`)
+})
